4bbc2a1ae9db1f27c5839b8dd73c35714210b5be
nathan <nathanjsim42@gmail.com>
2023-03-10 19:02:14 +0100

    ParamSplitter: added a failed() method to check if the splitter failed to parse the last argument (i.e. it reached the end of the string)

--- src/ParamSplitter.h
+++ src/ParamSplitter.h
@@ -19,6 +19,7 @@ public:
 	std::string	next_param();
 	std::string	peek_next_param()	{ std::string param = next_param(); reset(); return param; }
 	bool		reached_end() const { return m_pos >= m_param.size(); }
+	bool		failed()			{ return m_failed; }
 	void		reset()				{ m_pos = 0; }
 
 private:
@@ -28,22 +29,23 @@ private:
 
 	std::string	m_param;
 	index_type	m_pos;
+	bool		m_failed;
 };
 
 template<char separator>
 ParamSplitter<separator>::ParamSplitter(const std::string &parameter)
-	: m_param(parameter), m_pos(0)
+	: m_param(parameter), m_pos(0), m_failed(false)
 {
 }
 
 template<char separator>
 ParamSplitter<separator>::ParamSplitter(const Command& command, size_t command_parameter_index)
-	: m_param(command.get_parameters()[command_parameter_index]), m_pos(0)
+	: m_param(command.get_parameters()[command_parameter_index]), m_pos(0), m_failed(false)
 {
 }
 template<char separator>
 ParamSplitter<separator>::ParamSplitter(bool condition, const Command& command, size_t command_parameter_index)
-	: m_pos(0)
+	: m_pos(0), m_failed(false)
 {
 	if (condition)
 		m_param = command.get_parameters()[command_parameter_index];
@@ -52,8 +54,10 @@ ParamSplitter<separator>::ParamSplitter(bool condition, const Command& command, 
 template<char separator>
 std::string ParamSplitter<separator>::next_param()
 {
-	if (reached_end())
+	if (reached_end()) {
+		m_failed = true;
 		return "";
+	}
 
 	index_type new_pos = find_separator();
 	std::string new_param = m_param.substr(m_pos, new_pos - m_pos);
