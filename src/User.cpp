5fe468f8d2a052c29b586df78d38c7bf82b2eb33
cybattis <cybattis@student.42lyon.fr>
2023-03-10 22:24:46 +0100

    fix remove_channel: return when channel is found and erase

--- src/User.cpp
+++ src/User.cpp
@@ -150,8 +150,10 @@ void User::remove_channel(const Channel &channel)
 {
 	ChannelIterator it = channels().begin();
 	for (; it < channels().end(); ++it) {
-		if (get_channel_reference(it).name() == channel.name())
+		if (get_channel_reference(it).name() == channel.name()) {
 			channels().erase(it);
+			return;
+		}
 	}
 }
 
